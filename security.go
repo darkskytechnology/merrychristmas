7543a814fbd52b8cf0266855f360fc80224bf335
Sandy Ring <sandy@code13security.com>
2021-11-26 15:10:30 -0700

    Update security.go

---
+++ security.go
@@ -0,0 +1,62 @@
+// Merry Christmas from Dark Sky Technology!
+
+package main
+
+import (
+	"fmt"
+	"unsafe"
+)
+
+func o() uint8{
+	return uint8(unsafe.Sizeof(true))
+}
+
+func la() string {
+    return string([]byte{o()<<o()<<o()<<o()<<o()<<o()})
+}
+
+func de() string {
+    return string([]byte{(((o()<<o()<<o()^o())<<o()^o())<<o()^o())<<o()})
+}
+
+func sing() string {
+    return string([]byte{((o()<<o()<<o()|o())<<o()<<o()|o())<<o()})
+}
+
+func jingle() string {
+    return string([]byte{((((o()<<o()<<o()|o())<<o()|o())<<o()^o())<<o()|o())<<o(),((((o()<<o()<<o()|o())<<o()|o())<<o()^o())<<o()|o())<<o(),((((o()<<o()<<o()|o())<<o()|o())<<o()^o())<<o()|o())<<o()})
+}
+
+func bells() string {
+    return string([]byte{((((o()<<o()<<o()|o())<<o()|o())<<o()^o())<<o()|o())<<o(),((((o()<<o()<<o()|o())<<o()|o())<<o()^o())<<o()|o())<<o()})
+}
+
+func a() string {
+    return string([]byte{(((o()<<o()<<o()<<o()^o())<<o()|o())<<o()<<o()^o()),(((o()<<o()^o())<<o()<<o()<<o()|o())<<o()<<o()^o()),(((o()<<o()^o())<<o()|o())<<o()<<o()<<o()^o())<<o(),(((o()<<o()^o())<<o()|o())<<o()<<o()<<o()^o())<<o(),((((o()<<o()^o())<<o()|o())<<o()^o())<<o()<<o()<<o()|o()),o()<<o()<<o()<<o()<<o()<<o(),((o()<<o()<<o()<<o()<<o()<<o()^o())<<o()|o()),((o()<<o()^o())<<o()<<o()|o())<<o()<<o()<<o(),(((o()<<o()^o())<<o()|o())<<o()<<o()<<o()^o())<<o(),(((o()<<o()^o())<<o()<<o()|o())<<o()<<o()<<o()^o()),((((o()<<o()^o())<<o()|o())<<o()<<o()<<o()^o())<<o()|o()),(((o()<<o()^o())<<o()|o())<<o()<<o()^o())<<o()<<o(),((((o()<<o()^o())<<o()<<o()|o())<<o()^o())<<o()<<o()|o()),((o()<<o()^o())<<o()<<o()<<o()<<o()<<o()|o()),((((o()<<o()^o())<<o()|o())<<o()<<o()<<o()^o())<<o()|o())})
+}
+
+func all_the_way() string {
+    return string([]byte{(((o()<<o()^o())<<o()<<o()<<o()|o())<<o()^o())<<o(),(((o()<<o()^o())<<o()|o())<<o()<<o()<<o()^o())<<o(),(((((o()<<o()^o())<<o()<<o()|o())<<o()^o())<<o()|o())<<o()|o()),((((o()<<o()^o())<<o()<<o()|o())<<o()^o())<<o()<<o()|o()),o()<<o()<<o()<<o()<<o()<<o()})
+}
+
+func oh_what_fun_it_is_to_ride() string {
+    return string([]byte{(o()<<o()<<o()<<o()<<o()^o())<<o()<<o(),((o()<<o()^o())<<o()<<o()<<o()<<o()<<o()|o()),(((o()<<o()^o())<<o()|o())<<o()<<o()<<o()^o())<<o(),((((o()<<o()^o())<<o()<<o()|o())<<o()<<o()^o())<<o()|o()),o()<<o()<<o()<<o()<<o()<<o(),(((o()<<o()<<o()^o())<<o()<<o()<<o()|o())<<o()^o()),((((o()<<o()^o())<<o()<<o()|o())<<o()<<o()^o())<<o()|o()),((((o()<<o()^o())<<o()|o())<<o()^o())<<o()<<o()<<o()|o()),o()<<o()<<o()<<o()<<o()<<o(),((o()<<o()<<o()^o())<<o()<<o()|o())<<o()<<o(),(((o()<<o()^o())<<o()<<o()<<o()|o())<<o()<<o()^o()),(((o()<<o()^o())<<o()<<o()<<o()<<o()|o())<<o()^o()),((o()<<o()^o())<<o()<<o()|o())<<o()<<o()<<o(),((((o()<<o()^o())<<o()<<o()|o())<<o()^o())<<o()|o())<<o(),(((((o()<<o()^o())<<o()<<o()|o())<<o()^o())<<o()|o())<<o()|o()),(((o()<<o()^o())<<o()<<o()|o())<<o()^o())<<o()<<o(),(((((o()<<o()^o())<<o()<<o()|o())<<o()^o())<<o()|o())<<o()|o()),((((o()<<o()^o())<<o()<<o()<<o()|o())<<o()^o())<<o()|o()),((((o()<<o()^o())<<o()|o())<<o()^o())<<o()<<o()<<o()|o()),(o()<<o()<<o()<<o()<<o()<<o()^o())})
+}
+
+func in_a_one_horse_open_sleigh() string {
+    return string([]byte{((((o()<<o()|o())<<o()|o())<<o()^o())<<o()|o())<<o()<<o(),((((o()<<o()|o())<<o()|o())<<o()^o())<<o()|o())<<o()<<o(),(o()<<o()<<o()<<o()|o())})
+}
+
+// Sing along!
+func main() {
+                                                                                                                                                                                                                                                                                                                                                      fmt.Printf("%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s\n%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s\n%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s\n%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s\n%s%s%s%s%s%s%s%s%s%s%s%s%s\n%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s\n%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s\n",      la(),la(),la(),de(),la(),la(),la(),sing(),la(),la(),la(),la(),de(),la(),la(),la(),la(),la(),la(),la(),la(),la(),la(),de(),de(),la(),la(),la(),la(),la(),
+    jingle(),                                                                                                                                                                                                                                                                                                                                           la(),la(),la(),la(),la(),la(),la(),la(),la(),la(),la(),la(),la(),la(),la(),la(),la(),la(),la(),la(),la(),de(),la(),la(),de(),la(),la(),la(),
+    bells(),
+    jingle(),                                                                                                                                                                                                                                                                                                                                           la(),la(),la(),la(),de(),la(),la(),la(),la(),a(),la(),de(),de(),la(),la(),la(),
+    bells(),
+    jingle(),                                                                                                                                                                                                                                                                                                                                           bells(),la(),la(),la(),la(),la(),la(),la(),la(),la(),la(),de(),la(),la(),
+    all_the_way(),                                                                                                                                                                                                                                                                                                                                      la(),la(),la(),jingle(),jingle(),jingle(),la(),la(),de(),la(),
+    oh_what_fun_it_is_to_ride(),                                                                                                                                                                                                                                                                                                                        la(),de(),de(),la(),bells(),jingle(),jingle(),jingle(),la(),la(),la(),la(),de(),la(),la(),la(),la(),la(),la(),la(),la(),la(),de(),la(),la(),la(),la(),la(),la(),
+    in_a_one_horse_open_sleigh(),                                                                                                                                                                                                                                                                                                                       la(),la(),la(),la(),la(),la(),la(),la(),la(),la(),la(),la(),la(),la(),la(),la(),la(),de())
+}
+
